7d02bd854123dc1ddee114700badb11158b5a8e8
michaelk10452 <mskurdah@uci.edu>
2021-01-20 23:42:28 -0800

    Tab Bar addition for Superhero-related films

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Michael Kurdahi on 1/20/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
